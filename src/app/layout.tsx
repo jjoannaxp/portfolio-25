62d244e8166b8a4ef20344c1be93f8c55fcf272c
Joanna Poon <jhpoon@usc.edu>
2024-12-23 23:44:44 -0800

    landing hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,58 @@
+"use client";
 
 import "./globals.css";
+import { usePathname } from "next/navigation";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => (pathname === path ? "active" : "");
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
       <body>
         <header>
           <nav>
-            <a href="/">Home</a> | <a href="/about">About</a> | <a href="/just-for-fun">Just For Fun</a>
+            <a href="/">
+              <img
+                src="/logo.svg"
+                alt="Logo"
+                style={{ width: "32px", height: "32px" }}
+              />
+            </a>
+            <div className="links">
+              <a href="/" className={isActive("/")}>
+                Home
+              </a>
+              <a href="/about" className={isActive("/about")}>
+                About
+              </a>
+              <a href="/just-for-fun" className={isActive("/just-for-fun")}>
+                Just For Fun
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1DdgikEMpW-M7nETFOQ0Oip6tuMuJE5fi/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume ↗
+              </a>
+            </div>
           </nav>
         </header>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,50 @@
-
-
 export default function Home() {
   return (
     <main>
-      <h2>Welcome to My Portfolio</h2>
-      <p>Explore my work and learn more about me!</p>
+      <div className="hero">
+        <div id="hero-title">
+          <div>
+            Hey, I’m <em id="name">Joanna!</em>
+          </div>
+          <img
+            src="/logo.svg"
+            alt="Logo"
+            style={{ width: "32px", height: "32px" }}
+          />
+        </div>
+
+        <div id="tagline">
+          A product designer <span className="red">˚⊹</span> blending pixels &
+          code <span className="red">⊹₊⟡⋆</span> to build communities and tell
+          stories <span className="red">( ˃ᴗ˂) ✧*｡</span>
+        </div>
+      </div>
+      <div id="statuses">
+        <div className="mono">
+          <b>Previously:</b> Product Design @ Measured, WhatNot, and
+          Eightfold.AI
+        </div>
+        <div className="mono">
+          <b>Currently:</b> Building design communities, learning React, and
+          finding new LA cafes.{" "}
+        </div>
+      </div>
       <ul>
-        <li><a href="/projects/tinker">Tinker</a></li>
-        <li><a href="/projects/measured-kat">Known Audience Testing</a></li>
-        <li><a href="/projects/measured-design-system">Design System</a></li>
-        <li><a href="/projects/eightfold-ai">Eightfold.AI</a></li>
-        <li><a href="/projects/whatnot">WhatNot</a></li>
+        <li>
+          <a href="/projects/tinker">Tinker</a>
+        </li>
+        <li>
+          <a href="/projects/measured-kat">Known Audience Testing</a>
+        </li>
+        <li>
+          <a href="/projects/measured-design-system">Design System</a>
+        </li>
+        <li>
+          <a href="/projects/eightfold-ai">Eightfold.AI</a>
+        </li>
+        <li>
+          <a href="/projects/whatnot">WhatNot</a>
+        </li>
       </ul>
     </main>
   );
